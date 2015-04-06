d07c8515a066df1c980525f823cca80524c5bc0a
Jason Liu <jasonvbc@gmail.com>
2015-04-06 16:25:13 -0700

    Add AngularJS Route

--- assignment02_code/js/app.js
+++ assignment02_code/js/app.js
@@ -1,10 +1,26 @@
 /**
  * Created by Jason on 4/6/2015.
  */
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'views/mainview.html',
+            controller: 'mainController'
+        })
+
+        .when('/result', {
+            templateUrl: 'views/resultview.html',
+            controller: 'resultController'
+        })
+});
 
 myApp.controller('mainController', ['$scope', '$location', function($scope, $location){
 
     $scope.title = 'AngularJS Custom Directive - Form Submit';
 
 }]);
+
